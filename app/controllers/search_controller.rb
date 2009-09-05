f89c419b3f1f211a372e892efe8eab93a4fee9cc
matt.todd <matt.c.todd@gmail.com>
2009-09-05 08:16:09 -0500

    added search page and results page
    	modified:   app/controllers/search_controller.rb
    	new file:   app/views/search/index.rhtml
    	new file:   app/views/search/results.rhtml

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,9 @@
 class SearchController < ApplicationController
   def index
-    render :text => <<END
-      this is the beginning of my text...<br/>
-      this is another line
-      this is yet another line
-      and still another
-END
+  end
+
+  def results
+    @query = params[:query]
+    @results = ["blah", "blah blah"]
   end
 end
